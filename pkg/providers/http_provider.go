5431f35b0dc8c4f05b558e4ee0731604c1e5f21d
Luke Milby <luke.milby@gmail.com>
2026-02-19 15:29:42 -0500

    add license header to http_provider.go

--- pkg/providers/http_provider.go
+++ pkg/providers/http_provider.go
@@ -1,3 +1,9 @@
+// PicoClaw - Ultra-lightweight personal AI agent
+// Inspired by and based on nanobot: https://github.com/HKUDS/nanobot
+// License: MIT
+//
+// Copyright (c) 2026 PicoClaw contributors
+
 package providers
 
 import (
