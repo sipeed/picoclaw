fd99e55f2b7dea436f0cb0f12633c40d8bcb7e75
westwind027 <220070570@seu.edu.cn>
2026-02-28 21:19:26 +0800

    tools: shell improvements (restore from stash)

--- pkg/tools/shell.go
+++ pkg/tools/shell.go
@@ -103,7 +103,7 @@ func NewExecToolWithConfig(workingDir string, restrict bool, config *config.Conf
 
 	return &ExecTool{
 		workingDir:          workingDir,
-		timeout:             60 * time.Second,
+		timeout:             10 * time.Minute,
 		denyPatterns:        denyPatterns,
 		allowPatterns:       nil,
 		restrictToWorkspace: restrict,
