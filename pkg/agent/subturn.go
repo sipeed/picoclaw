e3d26b57150dc189ff67c4c75049f2ebbb62c954
Cytown <cytown@gmail.com>
2026-04-01 23:36:26 +0800

    add missing recover panic in subturn.go

--- pkg/agent/subturn.go
+++ pkg/agent/subturn.go
@@ -511,6 +511,7 @@ func deliverSubTurnResult(al *AgentLoop, parentTS *turnState, childID string, re
 	// We use defer/recover to catch any unlikely channel panics if it were ever closed.
 	defer func() {
 		if r := recover(); r != nil {
+			logger.RecoverPanicNoExit(r)
 			logger.WarnCF("subturn", "recovered panic sending to pendingResults", map[string]any{
 				"parent_id": parentTS.turnID,
 				"child_id":  childID,
